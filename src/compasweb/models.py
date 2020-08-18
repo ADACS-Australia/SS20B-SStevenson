ff11d87e66c76911f97b225145ac6bac885f3973
Stefan Oslowski <stefanoslowski@swin.edu.au>
2020-08-19 09:32:22 +1000

    Create first basic job model
    Summary:
    This model includes some basic metadata.
    Corresponding migration is included
    
    Test Plan: verify runserver still functions
    
    Reviewers: tiger, emanehab99
    
    Reviewed By: tiger
    
    Differential Revision: https://phab.gw-cloud.org/D104

--- src/compasweb/models.py
+++ src/compasweb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class COMPASJob(models.Model):
+    author = models.CharField(max_length=255, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    version = models.IntegerField()
+    public = models.BooleanField(default=False)
+    download_link = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ("name", "version")
