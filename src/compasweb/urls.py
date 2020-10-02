90c282be765b2d7c7d2d6cede784b6a51a2f49de
tiger <jitinghu@swin.edu.au>
2020-10-02 13:11:24 +1000

    create homepage
    Summary:
    header: get all the components listed, but the layout when page collapses needs to be fixed; box over link after clicking it needs to be fixed;
    homepage.content: get the basic layout;
    
    Test Plan: homepage styling
    
    Reviewers: mr-asher, emanehab99
    
    Reviewed By: emanehab99
    
    Differential Revision: https://phab.adacs.org.au/D204

--- src/compasweb/urls.py
+++ src/compasweb/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path, re_path
 from .views.job.jobs import KeywordView, JobDetailView
+from .views.common import index
 
 urlpatterns = [
-    url(r"^$", KeywordView.as_view(), name="jobtable"),
+    url(r"^$", index, name="homepage"),
+    url(r"^/published/", KeywordView.as_view(), name="jobtable"),
     path("<str:keyword_filter>/", KeywordView.as_view(), name="jobtable"),
     path(r"<int:pk>", JobDetailView.as_view(), name="job_detail"),
 ]
---
+++ src/compasweb/views/common.py
@@ -0,0 +1,36 @@
+# import logging
+# logger = logging.getLogger(__name__)
+# from django.http import Http404
+# from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+# from django.db.models import Q
+# from django.shortcuts import redirect, get_object_or_404
+# from django.core.paginator import Paginator
+
+from django.shortcuts import render
+from django.views import generic
+
+def index(request):
+    """
+    Render the index view.
+    :param request: Django request object.
+    :return: Rendered template
+    """
+    return render(
+        request,
+        "compasweb/common/index.html",
+    )
+
+# def job_table(request):
+#
+#     current_jobs = COMPASJob.objects.order_by('year')
+#     keyword_list = Keyword.objects.order_by('tag')
+#
+#     return render(
+#         request,
+#         "compasweb/job/job_table.html",
+#         {
+#             'jobs': current_jobs,
+#             'keywords': keyword_list
+#         }
+#     )
